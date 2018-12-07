1ceefbeaf241dfacd7048af7cbd915bc5d8fe4d1
Alexandra Brown <alexbro@umich.edu>
2018-12-07 10:21:43 -0500

    Add main function and command line arg

--- waldo-solver.py
+++ waldo-solver.py
@@ -4,9 +4,10 @@ from PIL import Image
 import time
 from functools import reduce
 from collections import Counter
+import sys
 
 def convertImageToArray(filename):
-    imageFilePath = 'images/' + filename + '.png'
+    imageFilePath = 'images/' + filename
     i = Image.open(imageFilePath)
     imageArray = numpy.asarray(i)
     return imageArray
@@ -14,3 +15,11 @@ def convertImageToArray(filename):
 def writeArrayToFile(imageFilename):
     writeFile = open('images/' + imageFilename + '.txt','a')
     writeFile.write(str(convertImageToArray(imageFilename).tolist()))
+
+
+def main():
+    imageFilename = sys.argv[1]
+    writeArrayToFile(imageFilename)
+
+if __name__ == '__main__':
+        main()
