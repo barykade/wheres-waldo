1ccefc4c34f6bd2f5e488c394cdb3e9e1f4d3171
Jake Anderson <barykade@umich.edu>
2018-12-07 11:42:24 -0500

    compares waldo-0 to top left corner of puzzle

--- waldo-solver.py
+++ waldo-solver.py
@@ -21,7 +21,9 @@ def writeArrayToFile(imageFilename):
 def compareTwoFilesOfSameSize(file1, file2):
     imageArray1 = convertImageToArray(file1)
     imageArray2 = convertImageToArray(file2)
+    compareTwoImageArrays(imageArray1, imageArray2)
 
+def compareTwoImageArrays(imageArray1, imageArray2):
     if ((len(imageArray1) != len(imageArray2)) or (len(imageArray1[0]) != len(imageArray2[0]))):
         print("error: images not same size")
         return
@@ -31,7 +33,7 @@ def compareTwoFilesOfSameSize(file1, file2):
         for column in range(len(imageArray1[row])):
             pixel1 = imageArray1[row][column]
             pixel2 = imageArray2[row][column]
-            if (pixel1[3] != 255 or pixel2[3] != 255):
+            if ((len(pixel1) == 4 and pixel1[3] != 255) or (len(pixel2) == 4 and pixel2[3] != 255)):
                 continue
             rCloseness = abs(int(pixel1[0]) - int(pixel2[0]))
             gCloseness = abs(int(pixel1[1]) - int(pixel2[1]))
@@ -40,10 +42,17 @@ def compareTwoFilesOfSameSize(file1, file2):
 
     print(counter)
 
+def getImageChunkFromImageArray(imageArray, startX, startY, size):
+    return imageArray[startX:startX+size, startY:startY+size]
+
+def compareImageToPuzzle(imageFilename, puzzleFilename):
+    imageArray = convertImageToArray(imageFilename)
+    puzzleArray = convertImageToArray(puzzleFilename)
+    puzzleChunk = getImageChunkFromImageArray(puzzleArray, 0, 0, len(imageArray))
+    compareTwoImageArrays(imageArray, puzzleChunk)
+
 def main():
-    imageFilename = sys.argv[1]
-    imageFilename2 = sys.argv[2]
-    compareTwoFilesOfSameSize(imageFilename, imageFilename2)
+    compareImageToPuzzle('waldo-0.png', sys.argv[1])
 
 if __name__ == '__main__':
         main()
